29318ac5acf2a1aff05fa3282b4a1d13080ae904
Andres B <andresblasiche@gmail.com>
2020-09-21 01:57:40 -0300

    add config DB connection

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import "reflect-metadata";
 import express from "express";
+import {createConnection} from "typeorm";
 import {ApolloServer} from "apollo-server-express";
 import * as dotEnv from "dotenv";
 
 //Set variables
 dotEnv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+//DB connection
+createConnection().then(connection => {
+	const app = express();
+	const PORT = process.env.PORT || 3000;
+	
+	app.listen(PORT, () => {
+		console.log(`Server listening on PORT: ${PORT}`);
+	});
+})
+.catch(error => {console.log(error)});
 
-app.listen(PORT, () => {
-	console.log(`Server listening on PORT: ${PORT}`);
-});
