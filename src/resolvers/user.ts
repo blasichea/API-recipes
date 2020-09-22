818183d9fff9cdfc93a502b4ed185493f1f901ed
Andres B <andresblasiche@gmail.com>
2020-09-22 16:38:05 -0300

    mutation signup

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,6 @@
 import {getRepository} from "typeorm";
 import {User} from "../entity/user";
+import * as bcrypt from "bcryptjs";
 
 export = {
 	Query: {
@@ -10,6 +11,24 @@ export = {
 		user: (_, {id}) => {
 			console.log("find one user");
 			return getRepository(User).findOne(id);
+		}
+	},
+	Mutation: {
+		signup: async (_, {input}) => {
+			const userRepository = getRepository(User);
+			try {
+				const user = await userRepository.findOne({email: input.email});
+				if (user) {
+					throw new Error('Email already in use');
+				}
+				const hashedPassword = await bcrypt.hash(input.password, 12);
+				const newUser = userRepository.create({...input, password: hashedPassword});
+				const result = await userRepository.save(newUser);
+				return result;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
 		}
 	}
 }
