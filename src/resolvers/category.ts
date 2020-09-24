266cba9789d64c6f978539825847f0defd75b582
Andres B <andresblasiche@gmail.com>
2020-09-24 14:20:15 -0300

    Query and Mutation Category

--- src/resolvers/category.ts
+++ src/resolvers/category.ts
@@ -1,8 +1,71 @@
 import {getRepository} from "typeorm";
+import {combineResolvers} from "graphql-resolvers";
+import {isAuthenticated} from "./middleware";
 import {Category} from "../entity/category";
+import { categoryTypeDefs } from "../typeDefs/category";
 
 export = {
 	Query: {
-		categories: () => getRepository(Category).find()
+		getCategories: combineResolvers(isAuthenticated, async () => {
+			try {
+				const result = await getRepository(Category).find({relations: ["recipes"]});
+				return result;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		}),
+		getOneCategory: combineResolvers(isAuthenticated, async (_, {id}) => {
+			try {
+				const category = await getRepository(Category).findOne(id, {relations: ["recipes"]});
+				if (!category) {
+					throw new Error('Category Not Found');
+				}
+				return category;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		}) 
+	},
+	Mutation: {
+		createCategory: combineResolvers(isAuthenticated, async (_, {input}) => {
+			try {
+				const findCategory = await getRepository(Category).findOne({name: input.name});
+				if (findCategory) {
+					throw new Error('This Category Name already exist');
+				}
+				const category = getRepository(Category).create({...input});
+				const result = getRepository(Category).save(category);
+				return result;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		}),
+		updateCategory: combineResolvers(isAuthenticated, async (_, {id, input}) => {
+			try {
+				await getRepository(Category).update(id, {...input});
+				const category = await getRepository(Category).findOne(id);
+				return category;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		}),
+		deleteCategory: combineResolvers(isAuthenticated, async (_, id) => {
+			try {
+				const category = await getRepository(Category).findOne(id, {relations: ["recipes"]});
+				console.log(category.recipes.length);
+				if(category.recipes.length > 0) {
+					throw new Error('Category is not empty');
+				}
+				await getRepository(Category).delete(id);
+				return category;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		})
 	}
 }
--- src/typeDefs/category.ts
+++ src/typeDefs/category.ts
@@ -2,12 +2,27 @@ import {gql} from "apollo-server-express";
 
 export const categoryTypeDefs = gql`
 	extend type Query {
-		categories: [Category!]
+		getCategories: [Category!]
+		getOneCategory(id: ID!): Category
+	}
+
+	extend type Mutation {
+		createCategory(input: createCategoryInput!): Category
+		updateCategory(id: ID!, input: updateCategoryInput!): Category
+		deleteCategory(id: ID!): Category
+	}
+
+	input createCategoryInput {
+		name: String!
+	}
+
+	input updateCategoryInput {
+		name: String
 	}
 
 	type Category {
 		id: ID!
 		name: String!
-		recipes: [Recipe!]
+		recipes: [Recipe!]!
 	}
 `;
